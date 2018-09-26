5834c632e8b24bb36e0473de8f16b416f8350bc3
zaggnut <42626902+zaggnut@users.noreply.github.com>
2018-09-26 11:38:17 -0700

    Update Super_Unix_Shell.cpp

--- Super_Unix_Shell.cpp
+++ Super_Unix_Shell.cpp
@@ -1,5 +1,5 @@
 /*
-Purpose: To create the Super Unix Shell™
+Purpose: To create the Super Unix Shell
 Creation Date: 9/26/2018
 Author(s): Shane Laskowski & (eventually Michael Lingo)
 Date Modified: 9/26/2018
